9817d9e5217c0a32f00b37bee2f21354b834171e
Sunnary2604 <50614684+Sunnary2604@users.noreply.github.com>
2021-08-31 14:16:26 +0800

    some update

--- drawRainChart.js
+++ drawRainChart.js
@@ -1,10 +1,26 @@
 export default class innerVerticalStack {
     constructor() {
-        this._svg=
+        this._svg = null;
 
+
+    }
+    svg(s) {
+        if (arguments.length === 0) return this._svg;
+        this._svg = s;
+        return this;
     }
 
     render() {
+        let width = 400;
+        let height = 400
+
+        var svg = d3
+            .select(".personal")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
 
     }
 }
